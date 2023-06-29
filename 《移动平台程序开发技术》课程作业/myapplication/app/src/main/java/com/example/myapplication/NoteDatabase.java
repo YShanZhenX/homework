993e693c8ab2d278bb7444054ba25b008e424f82
秦宇莉 <125621036+YShanZhenX@users.noreply.github.com>
2023-06-29 14:33:27 +0800

    Delete 《移动平台程序开发技术》课程作业/myapplication directory

--- 《移动平台程序开发技术》课程作业/myapplication/app/src/main/java/com/example/myapplication/NoteDatabase.java
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication;
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-@Database(entities = {Note.class},version = 1,exportSchema = false)
-public abstract class NoteDatabase extends RoomDatabase {
-    public abstract NoteDao getNoteDao();
-}
